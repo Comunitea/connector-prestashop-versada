899476ebedb2dbc6681833e0c64a47ec316be25d
javierjcf <javier@comunitea.com>
2025-06-19 12:08:42 +0200

    [ADD]connector_ecommerce con la dependencia del account.payment.mode de toda la vida

---
+++ connector_ecommerce/__manifest__.py
@@ -0,0 +1,36 @@
+# © 2013-2016 Camptocamp SA
+# © 2013-2016 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Connector for E-Commerce",
+    "version": "18.0.1.0.0",
+    "category": "Hidden",
+    "author": "Camptocamp,Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/connector-ecommerce",
+    "license": "AGPL-3",
+    "depends": [
+        # odoo
+        "stock_delivery",
+        # OCA/bank-payment-alternativeio
+        # "account_payment_base_oca_sale",
+        # OCA/bank-payment  (Para poder tener instalable account_payment_mode. Y no la nueva manera)
+        "account_payment_sale",
+        # OCA/connector
+        "connector_base_product",
+        # OCA/sale-workflow
+        "sale_exception",
+    ],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "wizard/sale_ignore_cancel_view.xml",
+        "data/ecommerce_data.xml",
+        "views/sale_order.xml",
+        "views/account_move.xml",
+        "views/stock_picking.xml",
+        "views/account_journal.xml",
+        "views/account_payment_mode.xml",
+    ],
+    "installable": True,
+}
